b3552a55c8d88344d2376baae03149514474973c
Sherlock Wisdom <wisdomnji@gmail.com>
2021-04-12 00:19:09 +0100

    Update: can send out SMS message, QR next

--- app/src/main/java/com/example/sw0b_001/MainActivity.java
+++ app/src/main/java/com/example/sw0b_001/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.sw0b_001;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,25 +25,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendBtn = findViewById(R.id.sendBtn);
+        sendBtn.setEnabled(false);
+
+        if( checkPermission(Manifest.permission.SEND_SMS)) {
+            sendBtn.setEnabled(true);
+        }
+        else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 1);
+            sendBtn.setEnabled(true);
+        }
     }
 
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.txt_original);
+        EditText eNumber = findViewById(R.id.editPhonenumber);
+        EditText eText = findViewById(R.id.editMessage);
 
-        String message = editText.getText().toString();
+        String number = eNumber.getText().toString();
+        String text = eText.getText().toString();
 
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        // TODO: check if valid input
+
+        if(checkPermission(Manifest.permission.SEND_SMS)) {
+            SmsManager smsManager = SmsManager.getDefault();
+
+            smsManager.sendTextMessage(number, null, text, null, null);
+            Toast.makeText(this, "Message sent!", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Toast.makeText(this, "Permission Denied!", Toast.LENGTH_LONG).show();
+        }
     }
 
-    public void viewMessage(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        EditText editText = (EditText) findViewById(R.id.txt_original);
+    public boolean checkPermission(String permission) {
+        int check = ContextCompat.checkSelfPermission(this, permission);
 
-        String message = editText.getText().toString();
-
-        TextView textView = findViewById(R.id.txt_originalView);
-        textView.setText(message);
+        return (check == PackageManager.PERMISSION_GRANTED);
     }
+
 }
