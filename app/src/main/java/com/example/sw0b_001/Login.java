7ec209944898eb373951533ed9d7cf0822225f72
Sherlock Wisdom <wisdomnji@gmail.com>
2021-04-17 01:16:44 +0100

    Update: Login screen included, and now intents. Will need some finetuning and everything will be good to go.

---
+++ app/src/main/java/com/example/sw0b_001/Login.java
@@ -0,0 +1,30 @@
+package com.example.sw0b_001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    EditText phonenumber, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void validateInput(View view) {
+        phonenumber = findViewById(R.id.user_phonenumber);
+        password = findViewById(R.id.user_password);
+
+        System.out.println("[+] Phonenumber:" + phonenumber.getText().toString());
+        System.out.println("[+] Password: " + password.getText().toString());
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
