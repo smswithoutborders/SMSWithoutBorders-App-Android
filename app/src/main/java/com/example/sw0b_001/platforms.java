f5b5dcce81e81cbbb3702c9de77c7ee412a29a4a
Sherlock Wisdom <wisdomnji@gmail.com>
2021-04-21 11:41:42 +0100

    Update: List item passes intent when clicked

--- app/src/main/java/com/example/sw0b_001/platforms.java
+++ app/src/main/java/com/example/sw0b_001/platforms.java
@@ -3,12 +3,16 @@ package com.example.sw0b_001;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -32,11 +36,24 @@ public class platforms extends AppCompatActivity {
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter((itemsAdapter));
 
-        itemsAdapter.add("Hello world 0");
-        itemsAdapter.add("Hello world 1");
-        itemsAdapter.add("Hello world 2");
-        itemsAdapter.add("Hello world 3");
-        itemsAdapter.add("Hello world 4");
+        itemsAdapter.add("[+] GOOGLE: gmail");
+
+        clickListener();
+    }
+
+    public void clickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Context context = getApplicationContext();
+                String clickedString = "Item just got clicked: [" + id + ":<"+ items.get(position)  + ">]";
+                Toast.makeText(context, clickedString, Toast.LENGTH_SHORT).show();
+
+
+                Intent intent = new Intent(parent.getContext(), sendMessageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 //    public void addItem(View view) {
