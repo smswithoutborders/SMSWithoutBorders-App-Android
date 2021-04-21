d1e659ff1844109facb881f1aadd2f468189a824
Sherlock Wisdom <wisdomnji@gmail.com>
2021-04-21 13:19:12 +0100

    Update: registerReceiver now works

--- app/src/main/java/com/example/sw0b_001/sendMessageActivity.java
+++ app/src/main/java/com/example/sw0b_001/sendMessageActivity.java
@@ -5,7 +5,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -17,6 +22,10 @@ import android.widget.Toast;
 public class sendMessageActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.sw0b_001.MESSAGE";
+
+    String SMS_SENT = "SENT";
+    String SMS_DELIVERED = "DELIVERED";
+    String SMS_FAILED = "FAILED";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +44,65 @@ public class sendMessageActivity extends AppCompatActivity {
         }
     }
 
+    public void smsFailed() {
+
+    }
+
     public void sendMessage(View view) {
         EditText eNumber = findViewById(R.id.editPhonenumber);
         EditText eText = findViewById(R.id.editMessage);
+
+        PendingIntent sentPendingIntent = PendingIntent.getBroadcast(this, 0, new Intent("SENT"), 0);
+        PendingIntent deliveredPendingIntent = PendingIntent.getBroadcast(this, 0, new Intent("DELIVERED"), 0);
+
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SMS_SENT));
+
+        //---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SMS_DELIVERED));
+
 
         String number = eNumber.getText().toString();
         String text = eText.getText().toString();
@@ -45,10 +110,12 @@ public class sendMessageActivity extends AppCompatActivity {
         // TODO: check if valid input
 
         if(checkPermission(Manifest.permission.SEND_SMS)) {
+
+
             SmsManager smsManager = SmsManager.getDefault();
 
-            smsManager.sendTextMessage(number, null, text, null, null);
-            Toast.makeText(this, "Message sent!", Toast.LENGTH_LONG).show();
+            smsManager.sendTextMessage(number, null, text, sentPendingIntent, deliveredPendingIntent);
+            Toast.makeText(this, "Sending SMS....", Toast.LENGTH_LONG).show();
         }
         else {
             Toast.makeText(this, "Permission Denied!", Toast.LENGTH_LONG).show();
