92bf7e41929f37f2aac4c59478b269e1cf30a9f9
Sherlock Wisdom <wisdomnji@gmail.com>
2021-05-21 23:32:53 +0100

    Update: barcode scans effectively now

--- app/src/main/java/com/example/sw0b_001/QRScannerActivity.java
+++ app/src/main/java/com/example/sw0b_001/QRScannerActivity.java
@@ -9,12 +9,15 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,8 +30,6 @@ import com.android.volley.toolbox.Volley;
 import com.example.sw0b_001.Helpers.Datastore;
 import com.example.sw0b_001.Helpers.Gateway;
 import com.example.sw0b_001.Helpers.SecurityLayer;
-import com.example.sw0b_001.Providers.Emails.EmailThreads;
-import com.example.sw0b_001.Providers.Emails.EmailThreadsDao;
 import com.example.sw0b_001.Providers.Platforms.PlatformDao;
 import com.example.sw0b_001.Providers.Platforms.Platforms;
 import com.google.android.gms.vision.CameraSource;
@@ -64,7 +65,8 @@ public class QRScannerActivity extends AppCompatActivity {
     private BarcodeDetector barcodeDetector;
     private CameraSource cameraSource;
     SurfaceView surfaceView;
-    TextView txtBarcodeValue;
+    private View loaderView;
+    private TextView syncText;
     private boolean requestingPermission = false;
 
     @Override
@@ -74,8 +76,12 @@ public class QRScannerActivity extends AppCompatActivity {
         System.out.println("[+] Back to the beginning...");
 
 //        scanButton = findViewById(R.id.scan_btn);
-        txtBarcodeValue = findViewById(R.id.qr_text);
-        surfaceView = findViewById(R.id.surfaceView);
+        surfaceView = findViewById(R.id.cameraView);
+        loaderView = findViewById(R.id.sync_loader);
+        syncText = findViewById(R.id.sync_status);
+
+        loaderView.setVisibility(View.GONE);
+        syncText.setVisibility(View.GONE);
 
         initialiseDetectorsAndSources();
     }
@@ -86,11 +92,11 @@ public class QRScannerActivity extends AppCompatActivity {
                 .build();
 
         cameraSource = new CameraSource.Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(1080, 1200)
+                .setRequestedPreviewSize(800, 800 )
                 .setAutoFocusEnabled(true) //you should add this feature
                 .build();
 
-        Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(), "Barcode scanner started", Toast.LENGTH_SHORT).show();
 
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
@@ -123,118 +129,112 @@ public class QRScannerActivity extends AppCompatActivity {
 
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
-//                Toast.makeText(getApplicationContext(), "QR code detected", Toast.LENGTH_SHORT).show();
-
+//                surfaceView.setVisibility(View.GONE);
                 final SparseArray<Barcode> barcodes = detections.getDetectedItems();
                 if (barcodes.size() != 0) {
                     barcodeDetector.release();
-                    txtBarcodeValue.post(new Runnable() {
+                    runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            System.out.println("[+] QR code detected");
-                            cameraSource.stop();
-                            SecurityLayer sl;
-                            try {
-                                sl = new SecurityLayer();
-
-                                Barcode.UrlBookmark intentData = barcodes.valueAt(0).url;
-                                System.out.println("\t[+]: " + intentData.url);
-
-                                String appOutput = intentData.url;
-                                txtBarcodeValue.setText(appOutput);
-
-                                appOutput += "\n[+] Transmitting public key... ";
-                                txtBarcodeValue.setText(appOutput);
-                                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-
-                                JSONObject jsonBody = new JSONObject("{\"public_key\": \"" + sl.init() + "\"}");
-                                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(intentData.url, jsonBody, new Response.Listener<JSONObject>() {
-                                        @Override
-                                        public void onResponse(JSONObject response) {
-                                            System.out.println("DONE: " + response.toString());
-                                            try {
-                                                String passwdHash = response.getString("pd");
-                                                String publicKey = response.getString("pk");
-                                                String sharedKey = response.getString("sk");
-                                                JSONArray platforms = response.getJSONArray("pl");
-                                                Log.i(this.getClass().getSimpleName(), "PasswdHash: " + passwdHash);
-                                                Log.i(this.getClass().getSimpleName(),"PublicKey: " + publicKey);
-                                                Log.i(this.getClass().getSimpleName(),"SharedKey: " + sharedKey);
-                                                Log.i(this.getClass().getSimpleName(),"Platforms: " + platforms);
-
-                                                Map<Integer, List<String>>[] extractedInformation = extractPlatformFromGateway(platforms);
-                                                Map<Integer, List<String>> providers = extractedInformation[0];
-                                                Map<Integer, List<String>> provider_platforms_map = extractedInformation[1];
-
-                                                storePlatformFromGateway(providers, provider_platforms_map);
-
-                                                byte[] decryptedSharedKey = sl.decrypt_RSA(sharedKey.getBytes("UTF-8"));
-                                                Log.i(this.getClass().getSimpleName(), "[+] Decrypted SharedKey: " + new String(decryptedSharedKey));
-
-                                                SharedPreferences app_preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                                                SharedPreferences.Editor editor = app_preferences.edit();
-                                                editor.putString(Gateway.VAR_PUBLICKEY, publicKey);
-                                                editor.putString(Gateway.VAR_PASSWDHASH, passwdHash);
-                                                editor.commit();
-                                                Intent logoutIntent = new Intent(getApplicationContext(), LoginActivity.class);
-                                                logoutIntent.putExtra("shared_key", sharedKey);
-//                                                logoutIntent.putExtra("public_key", sharedKey);
-                                                logout(logoutIntent);
-                                                finish();
-                                            } catch (JSONException e) {
-                                                e.printStackTrace();
-                                            } catch (BadPaddingException e) {
-                                                e.printStackTrace();
-                                            } catch (IllegalBlockSizeException e) {
-                                                e.printStackTrace();
-                                            } catch (InvalidKeyException e) {
-                                                e.printStackTrace();
-                                            } catch (UnsupportedEncodingException e) {
-                                                e.printStackTrace();
-                                            } catch (IOException e) {
-                                                e.printStackTrace();
-                                            }
-                                        }
-                                    }, new Response.ErrorListener() {
-                                        @Override
-                                        public void onErrorResponse(VolleyError error) {
-//                                            txtBarcodeValue.setText(appOutput);
-                                            System.out.println("Failed: " + error);
-//                                            if( error.networkResponse.statusCode == 500) {
-//                                                finish();
-//
-//                                                return;
-//                                            }
-                                        }
-                                });
-                                queue.add(jsonObjectRequest);
-                            } catch (KeyStoreException e) {
-                                e.printStackTrace();
-                            } catch (CertificateException e) {
-                                e.printStackTrace();
-                            } catch (NoSuchAlgorithmException e) {
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            } catch (BadPaddingException e) {
-                                e.printStackTrace();
-                            } catch (InvalidKeyException e) {
-                                e.printStackTrace();
-                            } catch (UnrecoverableKeyException e) {
-                                e.printStackTrace();
-                            } catch (InvalidAlgorithmParameterException e) {
-                                e.printStackTrace();
-                            } catch (NoSuchPaddingException e) {
-                                e.printStackTrace();
-                            } catch (NoSuchProviderException e) {
-                                e.printStackTrace();
-                            } catch (IllegalBlockSizeException e) {
-                                e.printStackTrace();
-                            }
+//                            surfaceView.setVisibility(View.GONE);
+//                            surfaceView.getBackground().setColorFilter(Color.rgb(123, 123, 123), PorterDuff.Mode.DARKEN);
+                            syncText.setVisibility(View.VISIBLE);
+                            loaderView.setVisibility(View.VISIBLE);
                         }
                     });
+                    SecurityLayer sl;
+                    try {
+                        sl = new SecurityLayer();
+
+                        Barcode.UrlBookmark intentData = barcodes.valueAt(0).url;
+                        System.out.println("\t[+]: " + intentData.url);
+
+                        String appOutput = intentData.url;
+                        syncText.setText(appOutput);
+
+                        appOutput += "\n[+] Transmitting public key... ";
+                        syncText.setText(appOutput);
+                        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+                        JSONObject jsonBody = new JSONObject("{\"public_key\": \"" + sl.init() + "\"}");
+                        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(intentData.url, jsonBody, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            System.out.println("DONE: " + response.toString());
+                            try {
+                                String passwdHash = response.getString("pd");
+                                String publicKey = response.getString("pk");
+                                String sharedKey = response.getString("sk");
+                                JSONArray platforms = response.getJSONArray("pl");
+                                Log.i(this.getClass().getSimpleName(), "PasswdHash: " + passwdHash);
+                                Log.i(this.getClass().getSimpleName(),"PublicKey: " + publicKey);
+                                Log.i(this.getClass().getSimpleName(),"SharedKey: " + sharedKey);
+                                Log.i(this.getClass().getSimpleName(),"Platforms: " + platforms);
+
+                                Map<Integer, List<String>>[] extractedInformation = extractPlatformFromGateway(platforms);
+                                Map<Integer, List<String>> providers = extractedInformation[0];
+                                Map<Integer, List<String>> provider_platforms_map = extractedInformation[1];
+
+                                storePlatformFromGateway(providers, provider_platforms_map);
+
+                                byte[] decryptedSharedKey = sl.decrypt_RSA(sharedKey.getBytes("UTF-8"));
+                                Log.i(this.getClass().getSimpleName(), "[+] Decrypted SharedKey: " + new String(decryptedSharedKey));
+
+                                SharedPreferences app_preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                                SharedPreferences.Editor editor = app_preferences.edit();
+                                editor.putString(Gateway.VAR_PUBLICKEY, publicKey);
+                                editor.putString(Gateway.VAR_PASSWDHASH, passwdHash);
+                                editor.commit();
+                                Intent logoutIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                                logoutIntent.putExtra("shared_key", sharedKey);
+                                logout(logoutIntent);
+                                finish();
+                            } catch (JSONException e) {
+                                    e.printStackTrace();
+                                } catch (BadPaddingException e) {
+                                    e.printStackTrace();
+                                } catch (IllegalBlockSizeException e) {
+                                    e.printStackTrace();
+                                } catch (InvalidKeyException e) {
+                                    e.printStackTrace();
+                                } catch (UnsupportedEncodingException e) {
+                                    e.printStackTrace();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                System.out.println("Failed: " + error);
+                            }
+                        });
+                        queue.add(jsonObjectRequest);
+                    } catch (KeyStoreException e) {
+                        e.printStackTrace();
+                    } catch (CertificateException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (BadPaddingException e) {
+                        e.printStackTrace();
+                    } catch (InvalidKeyException e) {
+                        e.printStackTrace();
+                    } catch (UnrecoverableKeyException e) {
+                        e.printStackTrace();
+                    } catch (InvalidAlgorithmParameterException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchPaddingException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchProviderException e) {
+                        e.printStackTrace();
+                    } catch (IllegalBlockSizeException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
@@ -288,6 +288,7 @@ public class QRScannerActivity extends AppCompatActivity {
         Map<Integer, List<String>>[] extractedInformation= new Map[]{providers, platforms};
         return extractedInformation;
     }
+
     @Override
     protected void onPause() {
         super.onPause();
