2938e345794c5989ea7f834a49c099a46c482c05
sherlock wisdom <wisdomnji@gmail.com>
2022-04-18 14:18:37 +0100

    update: updated platforms - ready to checkout alpha1

--- app/src/main/java/com/example/sw0b_001/Models/Platforms/PlatformsRecyclerAdapter.java
+++ app/src/main/java/com/example/sw0b_001/Models/Platforms/PlatformsRecyclerAdapter.java
@@ -40,7 +40,6 @@ public class PlatformsRecyclerAdapter extends RecyclerView.Adapter<PlatformsRecy
     @NotNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-//        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         LayoutInflater inflater = LayoutInflater.from(this.context);
         View view = inflater.inflate(this.customLayout, parent, false);
         return new ViewHolder(view);
@@ -51,7 +50,6 @@ public class PlatformsRecyclerAdapter extends RecyclerView.Adapter<PlatformsRecy
         Platforms platform = this.platforms.get(position);
         holder.name.setText(platform.getName());
         holder.description.setText(platform.getDescription());
-        holder.provider.setText(platform.getProvider());
         holder.type.setText(platform.getType());
 //        holder.image.setImageResource(platform.getImage());
 
