b06e259e938bce86349b01a1d4fcb0fca2d61033
steph237 <stephaniewnkwatoh@gmail.com>
2022-04-30 04:57:51 +0100

    commented out some code in the settings file

--- app/src/main/java/com/example/sw0b_001/SettingsActivity.java
+++ app/src/main/java/com/example/sw0b_001/SettingsActivity.java
@@ -14,7 +14,7 @@ import android.widget.RadioGroup;
 
 import com.example.sw0b_001.Database.Datastore;
 import com.example.sw0b_001.Providers.Gateway.GatewayDao;
-import com.example.sw0b_001.Providers.Gateway.GatewayPhonenumber;
+//import com.example.sw0b_001.Providers.Gateway.GatewayPhonenumber;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.jetbrains.annotations.NotNull;
@@ -25,13 +25,13 @@ public class SettingsActivity extends AppCompatActivity {
 
     RadioGroup radioGroup;
     BottomNavigationView bottomNavigationView;
-    List<GatewayPhonenumber> phonenumbers;
+//    List<GatewayPhonenumber> phonenumbers;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_new_settings2);
 
 
         bottomNavigationView = findViewById(R.id.homepage_bottom_navbar);
@@ -42,7 +42,7 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
 //                Log.i(this.getClass().getSimpleName(), item.getTitle().toString());
-                switch(item.getItemId()) {
+                switch (item.getItemId()) {
                     case R.id.platform:
                         startActivity(new Intent(getApplicationContext(), PlatformsActivity.class));
                         overridePendingTransition(0, 0);
@@ -53,25 +53,6 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
 
-        radioGroup = findViewById(R.id.settings_phonenumbers);
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                Datastore platformDb = Room.databaseBuilder(getApplicationContext(),
-                        Datastore.class, Datastore.DatabaseName).build();
-                GatewayDao gatewayDao = platformDb.gatewayDao();
-                phonenumbers = gatewayDao.getAll();
-            }
-        };
-        Thread dbFetchThread = new Thread(runnable);
-        dbFetchThread.start();
-        try {
-            dbFetchThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        loadSettings();
     }
 
 
@@ -87,60 +68,75 @@ public class SettingsActivity extends AppCompatActivity {
 
 
     private void loadSettings() {
-        for(GatewayPhonenumber phonenumber : phonenumbers) {
-            RadioButton button = new RadioButton(this);
-            button.setText("(" + phonenumber.getCountryCode() + ") " + phonenumber.getNumber() + "  |  " + phonenumber.getIsp());
-            button.setId((int) phonenumber.getId());
-            button.setTextSize(20);
-            if(phonenumber.isDefault())
-                button.setChecked(true);
-            button.setPadding(30, 50, 0, 50);
 
-
-//            button.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Thread makeDefault = new Thread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            Datastore platformDb = Room.databaseBuilder(getApplicationContext(),
-//                                    Datastore.class, Datastore.DatabaseName).build();
-//                            GatewayDao gatewayDao = platformDb.gatewayDao();
-//                            gatewayDao.updateDefault(true, phonenumber.getId());
-//                            gatewayDao.updateDefault(true, phonenumber.getId());
-//                        }
-//                    });
-//                }
-//            });
-            radioGroup.addView(button);
-        }
-
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                Thread makeDefault = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Datastore platformDb = Room.databaseBuilder(getApplicationContext(),
-                                Datastore.class, Datastore.DatabaseName).build();
-                        GatewayDao gatewayDao = platformDb.gatewayDao();
-                        gatewayDao.resetAllDefaults();
-                        gatewayDao.updateDefault(true, checkedId);
-                    }
-                });
-                makeDefault.start();
-                try {
-                    makeDefault.join();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 
-    @Override
-    public void onBackPressed() {
-        startActivity(new Intent(this, PlatformsActivity.class));
-        finish();
-    }
+
 }
+
+
+
+
+
+//this line of code controls the gateway client
+
+
+
+
+
+//    private void loadSettings() {
+//        for(GatewayPhonenumber phonenumber : phonenumbers) {
+//            RadioButton button = new RadioButton(this);
+//            button.setText("(" + phonenumber.getCountryCode() + ") " + phonenumber.getNumber() + "  |  " + phonenumber.getIsp());
+//            button.setId((int) phonenumber.getId());
+//            button.setTextSize(20);
+//            if(phonenumber.isDefault())
+//                button.setChecked(true);
+//            button.setPadding(30, 50, 0, 50);
+//
+//
+////            button.setOnClickListener(new View.OnClickListener() {
+////                @Override
+////                public void onClick(View v) {
+////                    Thread makeDefault = new Thread(new Runnable() {
+////                        @Override
+////                        public void run() {
+////                            Datastore platformDb = Room.databaseBuilder(getApplicationContext(),
+////                                    Datastore.class, Datastore.DatabaseName).build();
+////                            GatewayDao gatewayDao = platformDb.gatewayDao();
+////                            gatewayDao.updateDefault(true, phonenumber.getId());
+////                            gatewayDao.updateDefault(true, phonenumber.getId());
+////                        }
+////                    });
+////                }
+////            });
+//              radioGroup.addView(button);
+//        }
+//
+//        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                Thread makeDefault = new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        Datastore platformDb = Room.databaseBuilder(getApplicationContext(),
+//                                Datastore.class, Datastore.DatabaseName).build();
+//                        GatewayDao gatewayDao = platformDb.gatewayDao();
+//                        gatewayDao.resetAllDefaults();
+//                        gatewayDao.updateDefault(true, checkedId);
+//                    }
+//                });
+//                makeDefault.start();
+//                try {
+//                    makeDefault.join();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//    }
+//@Override
+//public void onBackPressed() {
+//    startActivity(new Intent(this, PlatformsActivity.class));
+//    finish();
+//}
