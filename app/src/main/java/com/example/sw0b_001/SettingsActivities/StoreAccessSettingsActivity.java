17e48ed134740190248b19ea842af720ff9e204b
sherlock wisdom <wisdomnji@gmail.com>
2022-05-24 14:31:10 +0100

    update: cleared logs and added beter recent UX

--- app/src/main/java/com/example/sw0b_001/SettingsActivities/StoreAccessSettingsActivity.java
+++ app/src/main/java/com/example/sw0b_001/SettingsActivities/StoreAccessSettingsActivity.java
@@ -13,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.example.sw0b_001.Models.EncryptedContent.EncryptedContentHandler;
 import com.example.sw0b_001.QRScannerActivity;
 import com.example.sw0b_001.R;
 import com.example.sw0b_001.SynchroniseTypeActivity;
@@ -28,12 +27,7 @@ public class StoreAccessSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_stored_access_settings);
     }
 
-    private void cleanseEncryptedContentDatabase() {
-        EncryptedContentHandler.clearedStoredEncryptedContents(getApplicationContext());
-    }
-
     public void onContinueClick(View view) {
-        cleanseEncryptedContentDatabase();
         String smswithoutbordersHandshakeUrl = "https://staging.smswithoutborders.com/login";
         Uri intentUri = Uri.parse(smswithoutbordersHandshakeUrl);
         Intent intent = new Intent(Intent.ACTION_VIEW, intentUri);
