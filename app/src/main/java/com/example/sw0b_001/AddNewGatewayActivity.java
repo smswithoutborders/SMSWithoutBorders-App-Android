1d2aad065132b3a060523a285ec187582386e896
steph237 <stephaniewnkwatoh@gmail.com>
2022-12-01 14:40:03 +0100

    update:opens new activity and now content

--- app/src/main/java/com/example/sw0b_001/AddNewGatewayActivity.java
+++ app/src/main/java/com/example/sw0b_001/AddNewGatewayActivity.java
@@ -2,7 +2,10 @@ package com.example.sw0b_001;
 
 
 
+import static com.example.sw0b_001.R.id.new_gateway_toolbar;
+
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -11,25 +14,21 @@ import androidx.appcompat.widget.Toolbar;
 import com.example.sw0b_001.Models.AppCompactActivityRtlEnabled;
 
 public class AddNewGatewayActivity extends AppCompactActivityRtlEnabled {
-
-    private int new_gateway_toolbar;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_gateway);
 
-        setContentView(R.layout.activity_add_gateway);
-
-//        Toolbar addNewGateway = findViewById(new_add_gateway_toolbar);
         Toolbar addNewGateway = findViewById(new_gateway_toolbar);
         setSupportActionBar(addNewGateway);
 
-        // Get a support ActionBar corresponding to this toolbar
+//         Get a support ActionBar corresponding to this toolbar
         ActionBar ab = getSupportActionBar();
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
-
-        
     }
 }
+
+
+
 
