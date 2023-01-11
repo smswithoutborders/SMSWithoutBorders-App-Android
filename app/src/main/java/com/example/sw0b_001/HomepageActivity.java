0cee426b82277ee2da07198b3322136f763ff7aa
sherlock wisdom <wisdomnji@gmail.com>
2023-01-11 17:15:22 +0100

    update: added an update for languages - should roll out

--- app/src/main/java/com/example/sw0b_001/HomepageActivity.java
+++ app/src/main/java/com/example/sw0b_001/HomepageActivity.java
@@ -1,6 +1,7 @@
 package com.example.sw0b_001;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
@@ -15,8 +16,17 @@ import com.example.sw0b_001.HomepageFragments.RecentsFragment;
 import com.example.sw0b_001.HomepageFragments.SettingsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.play.core.splitinstall.SplitInstallManager;
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory;
+import com.google.android.play.core.splitinstall.SplitInstallRequest;
+import com.google.android.play.core.splitinstall.SplitInstallSessionState;
+import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener;
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+import java.util.Set;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -62,19 +72,19 @@ public class HomepageActivity extends AppCompatActivity {
                         return true;
                     }
 
-                    case R.id.messages: {
-                        textView.setText(R.string.messages_title);
-                        textView.setVisibility(View.VISIBLE);
-                        fragmentManager.beginTransaction().replace(R.id.homepage_fragment_container_view,
-                                        NotificationsFragment.class, null)
-                                .setReorderingAllowed(true)
-                                .setCustomAnimations(android.R.anim.slide_in_left,
-                                        android.R.anim.slide_out_right,
-                                        android.R.anim.fade_in,
-                                        android.R.anim.fade_out)
-                                .commit();
-                        return true;
-                    }
+//                    case R.id.messages: {
+//                        textView.setText(R.string.messages_title);
+//                        textView.setVisibility(View.VISIBLE);
+//                        fragmentManager.beginTransaction().replace(R.id.homepage_fragment_container_view,
+//                                        NotificationsFragment.class, null)
+//                                .setReorderingAllowed(true)
+//                                .setCustomAnimations(android.R.anim.slide_in_left,
+//                                        android.R.anim.slide_out_right,
+//                                        android.R.anim.fade_in,
+//                                        android.R.anim.fade_out)
+//                                .commit();
+//                        return true;
+//                    }
                 }
                 return false;
             }
@@ -91,5 +101,33 @@ public class HomepageActivity extends AppCompatActivity {
                         android.R.anim.fade_in,
                         android.R.anim.fade_out)
                 .commit();
+    }
+
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        SplitInstallManager splitInstallManager = SplitInstallManagerFactory.create(getBaseContext());
+        final String[] supportedLanguages = getResources().getStringArray(R.array.language_values);
+        final Set<String> installedLangs = splitInstallManager.getInstalledLanguages();
+
+        for(String supportedLanguage : supportedLanguages) {
+            if(installedLangs.contains(supportedLanguage))
+                continue;
+
+            SplitInstallRequest request =
+                    SplitInstallRequest.newBuilder()
+                            .addLanguage(Locale.forLanguageTag(supportedLanguage))
+                            .build();
+            splitInstallManager.startInstall(request);
+            splitInstallManager.registerListener(new SplitInstallStateUpdatedListener() {
+                @Override
+                public void onStateUpdate(@NonNull SplitInstallSessionState splitInstallSessionState) {
+//                    if(splitInstallSessionState.status() == SplitInstallSessionStatus.INSTALLED){
+//                    }
+                }
+            });
+        }
     }
 }
