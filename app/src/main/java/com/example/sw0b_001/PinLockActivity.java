80dcdbddeda549aab320ae1cb5bd803481234325
steph237 <stephaniewnkwatoh@gmail.com>
2023-01-19 15:21:58 +0100

    fix: wanted to make the pin page the first activity

---
+++ app/src/main/java/com/example/sw0b_001/PinLockActivity.java
@@ -0,0 +1,14 @@
+package com.example.sw0b_001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PinLockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_lock2);
+    }
+}
