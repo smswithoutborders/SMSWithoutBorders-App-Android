86ce64ab7c135a6b5c319de7cc6de4f3609a6ead
sherlock <wisdomnji@gmail.com>
2024-03-16 23:41:11 +0100

    - update: cool unboarding dots

--- app/src/main/java/com/example/sw0b_001/OnboardingActivity.kt
+++ app/src/main/java/com/example/sw0b_001/OnboardingActivity.kt
@@ -2,7 +2,9 @@ package com.example.sw0b_001
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
 import android.view.View
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
@@ -14,6 +16,7 @@ import com.example.sw0b_001.Onboarding.ConnectivityStatusFragment
 import com.example.sw0b_001.Onboarding.OfflineExampleFragment
 import com.example.sw0b_001.Onboarding.WelcomeFragment
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textview.MaterialTextView
 
 class OnboardingActivity : AppCompatActivity() {
     private var fragmentIterator: MutableLiveData<Int> = MutableLiveData<Int>()
@@ -38,11 +41,23 @@ class OnboardingActivity : AppCompatActivity() {
     private fun configureButtonClicks() {
         val nextButton = findViewById<MaterialButton>(R.id.onboard_next_button);
         val prevButton = findViewById<MaterialButton>(R.id.onboard_back_button);
+        val dotIndicatorLayout = findViewById<LinearLayout>(R.id.onboard_dot_indicator_layout)
 
         fragmentIterator.observe(this) {
+            dotIndicatorLayout.removeAllViews()
+            val dots: Array<MaterialTextView> = Array(fragmentList.size) {
+                val materialTextView = MaterialTextView(applicationContext)
+                materialTextView.text = Html.fromHtml("&#8226", Html.FROM_HTML_MODE_LEGACY)
+                materialTextView.textSize = 35F
+                materialTextView.setTextColor(resources.getColor(R.color.pending_gray, theme))
+                dotIndicatorLayout.addView(materialTextView)
+                materialTextView
+            }
+            dots[it].setTextColor(resources.getColor(R.color.default_blue, theme))
             when {
                 it >= fragmentList.size - 1 -> {
                     nextButton.text = "Finish"
+                    TODO("[Localization] Add this to string.xml for translation")
                 }
                 else -> {
                     nextButton.text = "Next"
