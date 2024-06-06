254731ef9e1771077facdd552de8353fbea3939e
sherlock <wisdomnji@gmail.com>
2024-06-06 12:17:09 +0100

    - update: finally builds, not sure what is missing at this time

---
+++ app/src/androidTest/java/com/example/sw0b_001/gRPCTest.kt
@@ -0,0 +1,13 @@
+package com.example.sw0b_001
+
+import org.junit.Test
+
+class gRPCTest {
+
+    @Test
+    fun vaultV1Test() {
+//        val channel = ManagedChannelBuilder
+//            .forAddress("staging.smswithoutborders.com", 6000).usePlaintext().build()
+
+    }
+}
