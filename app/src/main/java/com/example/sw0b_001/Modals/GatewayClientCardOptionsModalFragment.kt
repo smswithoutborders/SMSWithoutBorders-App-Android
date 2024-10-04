cf86d90802c75d8aeca5c22eaf49d7c7767cc2cc
Ida Delphine <idadelm@gmail.com>
2024-10-04 13:25:33 +0100

    update: add button functionalities for gc modal

--- app/src/main/java/com/example/sw0b_001/Modals/GatewayClientCardOptionsModalFragment.kt
+++ app/src/main/java/com/example/sw0b_001/Modals/GatewayClientCardOptionsModalFragment.kt
@@ -1,14 +1,26 @@
 package com.example.sw0b_001.Modals
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.launch
+import androidx.fragment.app.viewModels
+import com.example.sw0b_001.Database.Datastore
 import com.example.sw0b_001.Models.GatewayClients.GatewayClient
+import com.example.sw0b_001.Models.GatewayClients.GatewayClientAddModalFragment
+import com.example.sw0b_001.Models.GatewayClients.GatewayClientViewModel
+import com.example.sw0b_001.Models.GatewayClients.GatewayClientsCommunications
 import com.example.sw0b_001.R
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class GatewayClientCardOptionsModalFragment(val gatewayClient: GatewayClient) : BottomSheetDialogFragment(R.layout.fragment_modal_gateway_client_card_options) {
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
+    private val viewModel: GatewayClientViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -21,6 +33,54 @@ class GatewayClientCardOptionsModalFragment(val gatewayClient: GatewayClient) : 
         val makeDefaultButton = view.findViewById<View>(R.id.make_default_button)
         val deleteButton = view.findViewById<View>(R.id.delete_button)
         val editButton = view.findViewById<View>(R.id.edit_button)
+
+        makeDefaultButton.setOnClickListener {
+            val gatewayClientsCommunications = GatewayClientsCommunications(requireContext())
+            gatewayClientsCommunications.updateDefaultGatewayClient(gatewayClient.mSISDN!!)
+            CoroutineScope(Dispatchers.Default).launch {
+                gatewayClient.type = GatewayClient.TYPE_CUSTOM
+                Datastore.getDatastore(it.context).gatewayClientsDao().update(gatewayClient)
+            }
+            Toast.makeText(requireContext(), "Default gateway client updated", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
+
+        editButton.setOnClickListener {
+            val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+            val gatewayClientAddFragment = GatewayClientAddModalFragment.newInstance(gatewayClient.id)
+            fragmentTransaction.add(gatewayClientAddFragment, "gateway_client_add_tag")
+            fragmentTransaction.show(gatewayClientAddFragment)
+            fragmentTransaction.commit()
+            dismiss()
+        }
+
+        deleteButton.setOnClickListener {
+            val defaultGatewayClientMsisdn = GatewayClientsCommunications(requireContext()).getDefaultGatewayClient()
+            if (defaultGatewayClientMsisdn == gatewayClient.mSISDN) {
+                AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.cannot_delete_selected_title)
+                    .setMessage(R.string.cannot_delete_selected_message)
+                    .setPositiveButton(R.string.ok) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            } else {
+                AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.delete_gateway_client_title)
+                    .setMessage(R.string.delete_gateway_client_message)
+                    .setPositiveButton(R.string.delete) { dialog, _ ->
+                        CoroutineScope(Dispatchers.IO).launch {
+                            viewModel.delete(requireContext(), gatewayClient)
+                        }
+                        dialog.dismiss()
+                        dismiss()
+                    }
+                    .setNegativeButton(R.string.cancel) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            }
+        }
 
 
     }
